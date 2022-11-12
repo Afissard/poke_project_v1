d9e6e39129e12306c7b7fc232d81888972ace194
Afissard <83972828+Afissard@users.noreply.github.com>
2022-11-12 16:22:06 +0100

    add more comment

--- src/main.c
+++ src/main.c
@@ -1,3 +1,14 @@
+/*
+To compile the project, just run the make.bat locate at the project root
+You can found the main.gb in the bin folder
+
+The ressources that I use :
+Github page of the GBDK 2020 lib : https://github.com/gbdk-2020/gbdk-2020
+YT playlist to learn how to program with GBDK 2020 : 
+https://www.youtube.com/playlist?list=PLeEj4c2zF7PaFv5MPYhNAkBGrkx4iPGJo
+
+*/
+
 #include<gb/gb.h>
 #include<stdio.h>
 
